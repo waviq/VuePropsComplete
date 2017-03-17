feea9b27fc3db1ac360d5b174e59535c4b3ef066
waviq <waviq.subkhi@gmail.com>
2017-03-17 23:29:02 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <appUser></appUser>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import User from './components/User.vue';
+
+    export default {
+        components: {
+            appUser: User
+        }
+    }
+</script>
+
+<style>
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{switchName()}}</p>
+        <p>User Age: {{userAge}}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name Function</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        //props with validation must String value
+        //props with atribute
+        //type: Object, Array, String, boolean, etc
+        props:{
+            myName: {
+                type: String,
+                required:true
+                /*default: function () {
+                    return {
+                        name: 'Waviq Default value props'
+                    }
+                }*/
+            },
+            resetFn:Function,
+            userAge: Number
+        },
+        methods:{
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = 'Waviq Reset Name'
+                this.$emit('nameWasReset', this.myName)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
